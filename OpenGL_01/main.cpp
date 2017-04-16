13738aa15ac82cb4c49bc70babf08b5ddac6e4fc
zhekazheka <tolstikovgk@gmail.com>
2017-04-16 19:56:19 +0200

    - initial commit

--- OpenGL_01/main.cpp
+++ OpenGL_01/main.cpp
@@ -6,10 +6,19 @@
 //  Copyright © 2017 HandMade. All rights reserved.
 //
 
-#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(int argc, const char * argv[])
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
     return 0;
 }
