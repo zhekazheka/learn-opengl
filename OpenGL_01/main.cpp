2cb63a0f8bcd024eceebbd765e23165fa59d9d1f
zhekazheka <tolstikovgk@gmail.com>
2017-04-16 12:57:35 +0200

    Initial Commit

---
+++ OpenGL_01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL_01
+//
+//  Created by Yevgeniy Tolstikov on 16/04/2017.
+//  Copyright © 2017 HandMade. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
