6f1f3091f8b5e2af9fb14f884b2ae16d4d147786
zhekazheka <tolstikovgk@gmail.com>
2017-04-19 23:35:45 +0200

    - implemented simple shader laoder class; - quick playing with vertex shader

--- OpenGL_01/main.cpp
+++ OpenGL_01/main.cpp
@@ -14,25 +14,7 @@
 
 #include <math.h>
 
-// Shaders
-const GLchar* vertexShaderSource =
-"#version 330 core\n"
-"layout (location = 0) in vec3 position;\n"
-"out vec4 vertexColor; // Specify a color output to the fragment shader\n"
-"void main()\n"
-"{\n"
-"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-"vertexColor = vec4(0.5f, 0.0f, 0.0f, 1.0f); // Set the output variable to a dark-red color\n"
-"}\0";
-
-const GLchar* fragmentShaderSource =
-"#version 330 core\n"
-"out vec4 color;\n"
-"uniform vec4 ourColor; // We set this variable in the OpenGL code.\n"
-"void main()\n"
-"{\n"
-"color = ourColor;\n"
-"}\n\0";
+#include "shader.h"
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
@@ -42,62 +24,15 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-GLuint shaderProgram;
 GLuint VAO, VBO, EBO;
-
-void initShaders()
-{
-    GLuint vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    
-    GLint success;
-    GLchar infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    
-    if(!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    
-    GLuint fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    
-    if(!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    
-    shaderProgram = glCreateProgram();
-    
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-}
 
 void setTriangleState()
 {
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+        // Positions         // Colors
+        0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // Bottom Right
+        -0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // Bottom Left
+        0.0f,  -0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // Top
     };  
 
     glGenVertexArrays(1, &VAO);
@@ -108,8 +43,12 @@ void setTriangleState()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
     
     glBindVertexArray(0);
 }
@@ -187,9 +126,11 @@ int main(int argc, const char * argv[])
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     
-    initShaders();
-//    setTriangleState();
-    setRectangleState();
+    Shader simpleShader("/Users/zhekazheka/Documents/HandMade/Projects/OpenGL/OpenGL_01/OpenGL_01/Shaders/Simple.ver",
+                        "/Users/zhekazheka/Documents/HandMade/Projects/OpenGL/OpenGL_01/OpenGL_01/Shaders/Simple.frag");
+    
+    setTriangleState();
+//    setRectangleState();
     
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     
@@ -206,14 +147,16 @@ int main(int argc, const char * argv[])
         glClear(GL_COLOR_BUFFER_BIT);
         
         GLfloat timeValue = glfwGetTime();
-        GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
-        GLfloat redValue = (sin(timeValue + 23) / 2) + 0.5;
-        GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUseProgram(shaderProgram);
-        glUniform4f(vertexColorLocation, redValue, greenValue, 0.0f, 1.0f);
+        GLfloat positionOffsetX = sin(timeValue) / 2;
+//        GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
+//        GLfloat redValue = (sin(timeValue + 23) / 2) + 0.5;
+        GLint offsetXLocation = glGetUniformLocation(simpleShader.Program, "offsetX");
+        simpleShader.Use();
+//        glUseProgram(shaderProgram);
+        glUniform1f(offsetXLocation, positionOffsetX);
         glBindVertexArray(VAO);
-//        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+//        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         
         glfwSwapBuffers(window);
